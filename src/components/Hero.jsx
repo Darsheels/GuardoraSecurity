bdb895ca8dc5183555afc2ff6e0651d1e0889875
Darsheel <darshgames.salecha@gmail.com>
2026-04-18 20:56:01 -0400

    Hero Section in the frontend added

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+export default function Hero() {
+    return (
+        <div className="Hero">
+            <h1 className="Title">Guardora Security</h1>
+            <div className="Subtitle">Instantly scan URLs and QR codes for phishing, malware, and unsafe redirects — powered by real-time threat intelligence.</div>
+
+            <div className="Hero-Items">
+                <div className="Hero-Item">QR Scan</div>
+                <div className="Hero-Item">URL Scan</div>
+            </div>
+
+        </div>
+    )
+
+}
