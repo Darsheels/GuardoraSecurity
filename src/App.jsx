28589558d8a3858668094a5e9fa1f9839cac82c9
Darsheel <darshgames.salecha@gmail.com>
2026-04-21 22:31:06 -0400

    QR Scanner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Hero from "./components/Hero"
 import Header from "./components/Header"
+import QRScanner from "./components/QR"
+
 
 export default function App() {
   return (
     <>
       <Header></Header>
       <Hero></Hero>
+      <QRScanner></QRScanner>
     </>
   )
 }
--- src/components/QR.jsx
+++ src/components/QR.jsx
@@ -1,13 +1,37 @@
 import Webcam from "react-webcam";
+import { useEffect,useRef } from "react";
 
-const videoConstraints = {
-    width: 400,
-    height: 400,
-    facingMode: "environment"
-};
 
 export default function QRScanner() {
+    const videoRef = useRef(null);
+    const streamRef = useRef(null);
+
+    useEffect(() => {
+        async function startCamera() {
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } });
+
+                streamRef.current = stream;
+                if (videoRef.current) {
+                    videoRef.current.srcObject = stream;
+                    await videoRef.current.play();
+                }
+            } catch (error) {
+                console.error("Error accessing camera:", error);
+                }      
+            }
+        startCamera();
+
+        return () => {
+            if (streamRef.current) {
+                streamRef.current.getTracks().forEach(track => track.stop());
+           }
+         };
+    }, []); 
+
     return (
-        <></>
-    )
+        <div className="QRScanner">
+            <video ref={videoRef} className="QRScanner-Video" autoPlay playsInline></video>
+        </div>
+    );
 }
