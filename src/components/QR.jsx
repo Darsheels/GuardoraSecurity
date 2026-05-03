4125003241fe891a52e320dd1f137822ac6e46fa
Darsheel <darshgames.salecha@gmail.com>
2026-05-02 22:23:00 -0400

    QR scanning logic added

--- src/components/QR.jsx
+++ src/components/QR.jsx
@@ -1,9 +1,9 @@
-import Webcam from "react-webcam";
 import { useEffect,useRef } from "react";
-
+import jsQR from "jsqr";
 
 export default function QRScanner() {
     const videoRef = useRef(null);
+    const canvasRef = useRef(null);
     const streamRef = useRef(null);
 
     useEffect(() => {
@@ -15,11 +15,39 @@ export default function QRScanner() {
                 if (videoRef.current) {
                     videoRef.current.srcObject = stream;
                     await videoRef.current.play();
+                    scanQRCode();
                 }
             } catch (error) {
                 console.error("Error accessing camera:", error);
-                }      
+                }
             }
+
+        function scanQRCode() {
+            const canvas = canvasRef.current;
+            const context = canvas.getContext("2d");
+
+            const scan = () => {
+                if (!videoRef.current) return;
+
+                canvas.width = videoRef.current.videoWidth;
+                canvas.height = video.current.videoHeight;
+
+                context.drawImage(videoRef.current,0,0, canvas.width,canvas.height);
+
+                const imageData = context.getImageData(0,0,canvas.width,canvas.height);
+
+                const code = jsQR(imageData.data, canvas.width, canvas.height);
+
+                if (code) {
+                    console.log("QR Code Found", code.data);
+                }
+
+                requestAnimationFrame(scan);
+            };
+
+            scan();
+        }
+
         startCamera();
 
         return () => {
@@ -31,7 +59,9 @@ export default function QRScanner() {
 
     return (
         <div className="QRScanner">
+            <h1 className="QRScanner-Title">QR Scanner:</h1>
             <video ref={videoRef} className="QRScanner-Video" autoPlay playsInline></video>
+            <canvas ref={canvasRef} style={{display: "none"}}></canvas>
         </div>
     );
 }
